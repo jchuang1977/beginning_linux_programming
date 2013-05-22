5f6e2d3a2f14f25a7d26474828995972d227bbd9
Andy Huang <jchuang1977@gmail.com>
2013-05-22 11:50:41 +0800

    initial commit

---
+++ app/create_tables.sql
@@ -0,0 +1,29 @@
+---
+--- SQL for creating the sample application database
+---
+
+-- Drop any existing tables
+drop table cd;
+drop table artist;
+drop table track;
+
+create table cd (
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	title VARCHAR(70) NOT NULL,	    
+	artist_id INTEGER NOT NULL,
+	catalogue VARCHAR(30) NOT NULL
+);
+
+create table artist (
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL
+);
+
+create table track (
+	cd_id INTEGER NOT NULL,
+	track_id INTEGER NOT NULL,
+	title VARCHAR(70),
+	PRIMARY KEY(cd_id, track_id)
+);
+
+
